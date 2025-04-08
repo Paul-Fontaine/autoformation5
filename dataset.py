dc8b4e77d3cc3c36a9be7d614e8e72639a4d151e
Paul Fontaine <pfontaine@etu.uqac.ca>
2025-04-08 14:02:30 -0400

    correct normalization

--- dataset.py
+++ dataset.py
@@ -4,14 +4,6 @@ from torchvision import transforms
 # Load the MNIST dataset
 transform = transforms.Compose([
     transforms.ToTensor(),
-    transforms.Normalize((0.1307,), (0.3081,))
+    transforms.Normalize(0.5, 0.5)
 ])
 mnist = MNIST(root='data', download=True, train=True, transform=transform)
-
-# Select only the digit '0' for training
-# mnist.data = mnist.data[mnist.targets == 0]
-# mnist.targets = mnist.targets[mnist.targets == 0]
-
-# select 5000 samples
-# mnist.data = mnist.data[:5000]
-# mnist.targets = mnist.targets[:5000]
